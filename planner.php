7de9e76de2c8b9fb33ea6d78ca4bc58105e10357
Unknown <dtcstudiointh@gmail.com>
2019-03-14 00:29:59 +0700

    fix

--- planner.php
+++ planner.php
@@ -14,12 +14,12 @@
     if (strpos($message, 'งาน') !== false) {
 
         $pieces = explode('งาน', $message);
-        $work = explode('ส่ง', $pieces[1])
+        $work = explode('ส่ง', $pieces[1]);
 
 
       $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = '$work[0]'.'$work[1]';
+        $arrayPostData['messages'][0]['text'] = $work[0].$work[1];
         replyMsg($arrayHeader,$arrayPostData);
     }
 
