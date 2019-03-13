ff8fc3560a2ccf68aa4bc8efbd676ca5177e72c1
Unknown <dtcstudiointh@gmail.com>
2019-03-13 23:40:16 +0700

    change

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'Y70P0dwqbPTdMtBmy47uA7eF//ENZXJqHb5mjLsrr4wEzv4U4vWX2lPOB/OpHi7BRnIaXlaaYa9ErKwVtbD9LGAvGYXCk+Z1j2A2DQ4LXOA7w307mQqdiadnU/KQLfH++gQRyIsXkdNQ9R3fxLSMaQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '72311211ef07b0e44fe9f44457398e57';
+$channelSecret = 'cba1729c4428c1c3c893046ec82d74ab';
 
 $pushID = 'U77bfaae6b2b30dbfc7913b1ac3f10eb9';
 
