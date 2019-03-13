16249b83796d719979ba0bda5a5a6ef2e1e4ee66
Unknown <dtcstudiointh@gmail.com>
2019-03-13 23:37:18 +0700

    test pust

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'Y70P0dwqbPTdMtBmy47uA7eF//ENZXJqHb5mjLsrr4wEzv4U4vWX2lPOB/OpHi7BRnIaXlaaYa9ErKwVtbD9LGAvGYXCk+Z1j2A2DQ4LXOA7w307mQqdiadnU/KQLfH++gQRyIsXkdNQ9R3fxLSMaQdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '72311211ef07b0e44fe9f44457398e57';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U77bfaae6b2b30dbfc7913b1ac3f10eb9';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
